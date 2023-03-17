02a4ac8a8617d1d34c7eb2a73478729dcd8649ee
thiagowaib <thiagowaib@gmail.com>
2023-03-16 21:23:21 -0300

    Finalizacao do Curso

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const webpush = require('web-push');
 
 const {listarSeguros, salvarSeguro} = require('./services/seguro-service')
+
+const vapidKeys = {
+    publicKey: 'BISH8_i_g_ifQeqZpzZld8InEd0fFNPzXuj-aKKPDdkNfiFzIpJgJI53PxroF2wBKpbC3RCndBdc7oCHR3AlxNk',
+    privateKey: 'fevnxjk3Hhex88hAQgMZ7D27tEpYDzrrvdHC02TUeYQ'
+}
+webpush.setVapidDetails(
+    'mailto:teste@email.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+)
 
 const app = express()
 app.use(bodyParser.json())
